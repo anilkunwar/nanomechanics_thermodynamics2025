1b414845e16d26cdb542401574b67397e95d0571
Anil Kunwar <anilkunwar318@yahoo.com>
2025-11-25 18:16:07 +0100

    Update phase_field_model_nanomaterial2d_r16.py

--- continuum_methods/phase_field_model_nanomaterial2d_r16.py
+++ continuum_methods/phase_field_model_nanomaterial2d_r16.py
@@ -361,7 +361,7 @@ def compute_stress_fields(etas, eps0, thetas):
     sxy = 2 * C44_p * (exy - eps_xy_star)
     # Stress fields in GPa
     sigma_mag = np.sqrt(sxx**2 + syy**2 + 2 * sxy**2) / 1e9
-    sigma_hydro = (sxx + syy) / 3 / 1e9
+    sigma_hydro = (sxx + syy) / 2 / 1e9
     # von Mises with plane strain szz
     szz = (C12 / (C11 + C12)) * (sxx + syy)  # Approximate
     von_mises = np.sqrt(0.5 * ((sxx - syy)**2 + (syy - szz)**2 + (szz - sxx)**2 + 6 * sxy**2)) / 1e9
