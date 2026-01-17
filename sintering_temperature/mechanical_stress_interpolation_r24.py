f6c5e618141da7a5fd010c56dd13ebae4dce0c11
Anil Kunwar <anilkunwar318@yahoo.com>
2026-01-17 01:18:42 +0100

    Update mechanical_stress_interpolation_r24.py

--- sintering_temperature/mechanical_stress_interpolation_r24.py
+++ sintering_temperature/mechanical_stress_interpolation_r24.py
@@ -727,7 +727,7 @@ class HeatMapVisualizer:
                 yaxis=dict(
                     title="Y Position",
                     #titlefont=dict(size=18, family='Arial', color='black'),
-                    title=dict(text="Title", font=dict(size=18, family="Arial", color="black"))
+                    title=dict(text="Title", font=dict(size=18, family="Arial", color="black")),
                     tickfont=dict(size=14, family='Arial'),
                     gridcolor='rgba(150, 150, 150, 0.3)',
                     scaleanchor="x",
