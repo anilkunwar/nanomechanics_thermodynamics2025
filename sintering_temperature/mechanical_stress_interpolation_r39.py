d01aec8b57103652979dbff299056f6ef1d599c2
Anil Kunwar <anilkunwar318@yahoo.com>
2026-01-23 02:02:45 +0100

    Update mechanical_stress_interpolation_r39.py

--- sintering_temperature/mechanical_stress_interpolation_r39.py
+++ sintering_temperature/mechanical_stress_interpolation_r39.py
@@ -1,4 +1,3 @@
-```python
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -2705,4 +2704,4 @@ def main():
 # =============================================
 if __name__ == "__main__":
     main()
-```
+
